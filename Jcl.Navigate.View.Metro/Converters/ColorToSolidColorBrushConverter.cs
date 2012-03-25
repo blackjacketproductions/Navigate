424c5213004b94cbbd27f3faddd53d9821e011fd
Jonathon Lee <blackjacketcriminals@gmail.com>
2012-03-25 13:25:10 +0100

    FIxed corruption

---
+++ Jcl.Navigate.View.Metro/Converters/ColorToSolidColorBrushConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Media;
+
+namespace Jcl.Navigate.View.Metro.Converters
+{
+    public class ColorToSolidColorBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var color = (Windows.UI.Color)value;
+
+            return new SolidColorBrush { Color = color };
+
+
+        }
+
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return null;
+        }
+
+
+
+    }
+}
